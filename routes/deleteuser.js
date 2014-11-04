7b2bd3a0a725c757d520c8e071cdd27b595b006a
Tinniam Ganesh <tv_ganesh@in.ibm.com>
2014-11-04 15:55:56 +0530

    Delete user

--- routes/deleteuser.js
+++ routes/deleteuser.js
@@ -5,6 +5,7 @@
  * File: deleteuser.js
  */
 var mongodb = require('mongodb');
+var async = require('async');
 
 //Create a callback function
 var mycallback = function(err,results) {
@@ -33,6 +34,7 @@ exports.list = function(req, res) {
 	var db= MongoClient.connect(mongo.url, function(err, db) {
 	  if(err) {
 	    console.log("Failed to connect to the database");
+	    return;
 	  } else {
 	    console.log("Connected to database");
 	  }
@@ -42,22 +44,60 @@ exports.list = function(req, res) {
 	var LastName = req.body.lastname;
 	var Mobile = req.body.mobile;
 
-    // Set our collection
-    var collection = db.collection('phonebook');
-
-    // Delete the specified record
-    collection.remove({"FirstName" : FirstName}    		
-    , function (err, doc) {
-        if (err) {
-            // If it failed, return error
-            res.send("There was a problem removing the information to the database.");
-        }
-        else {
-            // Redirect to userlist
-            res.location("userlist");
-            // And forward to success page
-            res.redirect("userlist");
-        }
-    });
+	 async.series([
+	                
+	                function(callback)
+	                { 
+	                	collection = db.collection('phonebook', function(error, response) {
+	              	      if( error ) {
+	              	          console.log(error + "  Could not connect to database-1")
+	              	          console.log("2");
+	              	          return;
+	              	          
+	              	       }
+	              	       else {
+	              	          console.log("Connected to phonebook");
+	              	          console.log("3");
+	              	       }
+	                	});
+	                	callback(null, 'one');
+	              	    
+	                },
+	                function(callback)
+	                {
+	                	console.log("222");
+	                	// Insert the record into the DB
+	                	// Delete the specified record
+	                	
+	                	collection.findOne(function(err, item) {
+	                		console.log("Item is ="+ item);
+	                         collection.remove(item, {justOne:true},(function (err, doc) {
+	                             if (err) {
+	                                 // If it failed, return error
+	                                 res.send("There was a problem removing the information to the database.");
+	                             }
+	                            else {
+	                               console.log("Removed------------")
+	                               // If it worked redirect to userlist
+	                               res.location("userlist");
+	                               // And forward to success page
+	                               res.redirect("userlist");
+	                           }
+	                          }));
+	                         
+	                       	
+	                	});
+	                	collection.find().toArray(function(err, items) {
+    	    	            console.log("Length =----------------" + items.length);
+    	    	            //db.close();
+                            });
+	                	
+	                	callback(null, 'two');
+	                }
+	               
+	             ]);
+   
+ 
 	});
+   
 };
